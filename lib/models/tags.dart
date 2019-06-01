5086d35d700a4ce50c44815c1a0aad5de88bbddb
Didier Boelens <mail@didierboelens.com>
2019-06-01 14:39:00 +0200

    Create tags.dart

---
+++ lib/models/tags.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_mentor/config/global_translations.dart';
+
+enum Tags {
+  fundamentals,
+  architecture,
+  animations,
+  hummingbird,
+  desktop,
+}
+
+class TagsHelper {
+  static String translation(Tags tag){
+    return allTranslations.text('tags.${tag.toString()}');
+  }
+}
