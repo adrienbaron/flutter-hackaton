9bdb09cab96c6d31c1fc2cd2b2baf01d27d02c85
adrienbaron <adrien@abaron.net>
2019-06-01 17:19:25 +0100

    Only show onboarding once

--- lib/application.dart
+++ lib/application.dart
@@ -3,11 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mentor/config/config.dart';
 import 'package:flutter_mentor/pages/authentication_registration/login_page.dart';
 import 'package:flutter_mentor/pages/home_page.dart';
+import 'package:flutter_mentor/pages/onboarding_page.dart';
 import 'package:flutter_mentor/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  Widget homePage;
+
   @override
   Widget build(BuildContext context) {
+    if (homePage == null) {
+      return Container();
+    }
+
     return MaterialApp(
         title: config.applicationName,
         debugShowCheckedModeBanner: false,
@@ -17,8 +30,7 @@ class Application extends StatelessWidget {
         ///
         routes: ApplicationRoutes.routes,
 
-//            home: OnBoardingPage(),
-        home: LoginPage(),
+        home: homePage,
         theme: ThemeData(
           // Define the default Brightness and Colors
           brightness: Brightness.dark,
@@ -28,5 +40,18 @@ class Application extends StatelessWidget {
           // Define the default Font Family
           fontFamily: 'Montserrat',
         ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    choseHomePage();
+  }
+
+  void choseHomePage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool hasSeenOnBoarding = prefs.getBool('oboarding.hasSeen');
+    homePage = hasSeenOnBoarding ? LoginPage() : OnBoardingPage();
   }
 }
