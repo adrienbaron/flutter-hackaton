26aa6cb1c0c5beb82e2529bef21e70c4af1fcf7b
Didier Boelens <mail@didierboelens.com>
2019-06-01 19:23:00 +0200

    dbo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -79,3 +79,75 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+class RequestCard extends StatelessWidget {
+  RequestCard({
+    Key key,
+    this.red,
+    this.green,
+    this.blue,
+    this.requestText,
+  }): super(key: key);
+
+  final int red;
+  final int green;
+  final int blue;
+  final String requestText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12,
+      color: Color.fromARGB(
+          255, red, green, blue),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Container(
+        width: 340,
+        height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blueGrey.shade500,
+              backgroundImage: AssetImage('images'),
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.black,
+                fontSize: 35,
+              ),
+            ),
+            Text(
+              'MY EMAIL',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.blueGrey.shade800,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Divider(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              requestText,
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.blueGrey.shade800,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
