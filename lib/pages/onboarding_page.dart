aa6e575741e1b53a65f3b0b74f93a91a2df489b1
adrienbaron <adrien@abaron.net>
2019-06-01 19:10:29 +0100

    Add icons to the on boarding

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -5,55 +5,67 @@ import 'package:fancy_on_boarding/page_model.dart';
 class OnBoardingPage extends StatelessWidget {
   final pageList = [
     PageModel(
-        color: const Color(0x88678FB4),
-        heroAssetPath: 'assets/images/student.png',
-        title: Text(
-          'Looking for a Mentor ?',
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Colors.white,
-            fontSize: 20.0,
-          ),
+      color: const Color.fromARGB(200, 103, 143, 180),
+      heroAssetPath: 'assets/images/student.png',
+      title: Text(
+        'Looking for a Mentor ?',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+          fontSize: 20.0,
         ),
-        body: Text('Here you find the one for you.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-            )),
-        iconAssetPath: 'assets/images/student.png'),
+      ),
+      body: Text(
+        'Here you find the one for you.',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+        ),
+      ),
+      iconAssetPath: 'assets/images/cap.png',
+    ),
     PageModel(
-        color: const Color(0x8865B0B4),
-        heroAssetPath: 'assets/images/teacher.png',
-        title: Text('Becoming a Mentor ?',
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              color: Colors.white,
-              fontSize: 20.0,
-            )),
-        body: Text('You can offer your services here.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-            )),
-        iconAssetPath: 'assets/images/teacher.png'),
+      color: const Color.fromARGB(200, 101, 176, 180),
+      heroAssetPath: 'assets/images/teacher.png',
+      title: Text(
+        'Becoming a Mentor ?',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+      body: Text(
+        'You can offer your services here.',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+        ),
+      ),
+      iconAssetPath: 'assets/images/info.png',
+    ),
     PageModel(
-      color: const Color(0x889B90BC),
+      color: const Color.fromARGB(200, 155, 144, 188),
       heroAssetPath: 'assets/images/path.png',
-      title: Text('My Path',
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Colors.white,
-            fontSize: 20.0,
-          )),
-      body: Text('You can find the right path for you',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-          )),
-      iconAssetPath: 'assets/images/path.png',
+      title: Text(
+        'My Path',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+      body: Text(
+        'You can find the right path for you',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+        ),
+      ),
+      iconAssetPath: 'assets/images/trophy.png',
     ),
   ];
 
