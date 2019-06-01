42c2c39fd5dcc9fa2213a53dcf0da74289f14a70
Didier Boelens <mail@didierboelens.com>
2019-06-01 20:10:41 +0200

    dbo

--- lib/models/tags.dart
+++ lib/models/tags.dart
@@ -7,3 +7,13 @@ enum Tags {
   hummingbird,
   desktop,
 }
+
+class TagsHelper {
+  static Tags fromText(String text){
+    Tags tags = Tags.fundamentals;
+
+    tags = Tags.values.firstWhere((e) => e.toString().split('.')[1] == text);
+ 
+    return tags;
+  }
+}
