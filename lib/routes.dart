1241435ff8e5c1a0d26e6729c5213c3c8698782d
wkidziak@gmail.com <Arupcode2018>
2019-06-01 12:38:26 +0100

    added home page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mentor/pages/home_page.dart';
 import 'package:flutter_mentor/pages/onboarding_page.dart';
 
 class ApplicationRoutes {
   static Map<String, WidgetBuilder> routes = {
     '/onboarding': (BuildContext context) => OnBoardingPage(),
+    '/home': (BuildContext context) => HomePage(),
   };
 }
